c5ccb17321d3a25f8e3f0582280d194238f48d50
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-08-27 00:18:31 +0530

    Add MegalodonScene

---
+++ ARVideo/Assets/MoveShark.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShark : MonoBehaviour {
+
+    float distanceToTravel = 1f;
+
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(transform.forward * distanceToTravel * Time.deltaTime); 
+	}
+}
